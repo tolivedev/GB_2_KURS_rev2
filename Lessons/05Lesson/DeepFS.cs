c7391a24269d1bd6d7483bf74799e9678cf15d77
tolivedev <tolive@live.ru>
2021-10-18 21:01:19 +0300

    prev2 les05

---
+++ Lessons/05Lesson/DeepFS.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons._05Lesson
+{
+    class DeepFS
+    {
+        public void DFS(int s)
+        {
+            bool[] visited = new bool[Vertices];
+
+            //For DFS use stack
+            Stack<int> stack = new Stack<int>();
+            visited[s] = true;
+            stack.Push(s);
+
+            while (stack.Count != 0)
+            {
+                s = stack.Pop();
+                Console.WriteLine("next->" + s);
+                foreach (int i in adj[s])
+                {
+                    if (!visited[i])
+                    {
+                        visited[i] = true;
+                        stack.Push(i);
+                    }
+                }
+            }
+        }
+}
